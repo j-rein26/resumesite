7b405ee4bbbaf811d33c44466d09db8938c14841
J.Reinhardt <68156978+j-rein26@users.noreply.github.com>
2024-05-15 18:48:08 +0300

    Update main.js
    add code to clear form when user pushes go back

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -253,6 +253,12 @@
       mirror: false
     })
   });
+  
+  window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
 
   /**
    * Initiate Pure Counter 
